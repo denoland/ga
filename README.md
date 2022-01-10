5662d2474e801d0f17688fdbfb3e66784ccb8229
Kitson Kelly <me@kitsonkelly.com>
2022-01-10 15:30:19 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# ga
+
+Utilities for server side processing of Google Analytics in Deno CLI and Deploy.
+
+When you server side render pages, it can be more efficient to not add Google
+Analytics to the client side app, and instead send the messages directly via
+your edge worker. This library provides a framework for doing this.
+
+## Usage
+
+The library is designed to generate a measure message for each request and
+response handled by a Deno CLI or Deno Deploy server. These messages are then
+queued up and asynchronously batched to Google Analytics.
+
+### `createReporter()`
+
+If you are using the Deno HTTP APIs directly, `std/http`, or various other HTTP
+frameworks, `createReporter()` will return a function which can be used to
+dispatch messages to Google Analytics.
+
+You need to create the reporter function, and then call the reporter with
+information about the current request and response.
+
+```ts
+import { createReporter } from "https://deno.land/x/ga/mod.ts";
+
+const reporter = createReporter();
+```
+
+If you are using the
+[low-level Deno API](https://deno.land/manual/runtime/http_server_apis_low_level)
+for HTTP servers, usage of the reporter would look something like this:
+
+```ts
+import { createReporter } from "https://deno.land/x/ga/mod.ts";
+
+const reporter = createReporter();
+
+for await (const conn of Deno.listen({ port: 0 })) {
+  (async () => {
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+      let err;
+      const start = performance.now();
+      try {
+        // processing of the request...
+        const response = new Response(/* response details */);
+        await requestEvent.respondWith(response);
+      } catch (e) {
+        err = e;
+      } finally {
+        await reporter(requestEvent.request, conn, response, start, err);
+      }
+    }
+  })();
+}
+```
+
+If you are using the
+[`std` library HTTP API](https://deno.land/manual@v1.17.2/runtime/http_server_apis)
+then it would look something like this:
+
+```ts
+import { createReporter } from "https://deno.land/x/ga/mod.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
+import type { ConnInfo } from "https://deno.land/std/http/server.ts";
+
+const reporter = createReporter();
+
+function handler(req: Request, conn: ConnInfo) {
+  let err;
+  let res: Response;
+  const start = performance.now();
+  try {
+    // processing of the request...
+    res = new Response(/* response details */);
+  } catch (e) {
+    err = e;
+  } finally {
+    reporter(req, conn, res!, start, err);
+  }
+  return res!;
+}
+
+serve(handler);
+```
+
+### `createReportMiddleware()`
+
+If you are using [oak](https://deno.land/x/oak/), then
+`createReportMiddleware()` can be used to create middleware which will
+
+```ts
+import { createReportMiddleware } from "https://deno.land/x/ga/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const report = createReportMiddleware();
+const app = new Application();
+
+app.use(report);
+// register additional middleware...
+
+app.listen({ port: 0 });
+```
---
+++ test.ts
@@ -0,0 +1,169 @@
+// Copyright 2021-2022 the Deno authors. All rights reserved. MIT license.
+
+import { delay } from "https://deno.land/std@0.120.0/async/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
+import {
+  createMockContext,
+  createMockNext,
+} from "https://deno.land/x/oak@v10.1.0/testing.ts";
+
+import { createReporter, createReportMiddleware } from "./mod.ts";
+
+const mockConn = {
+  remoteAddr: {
+    hostname: "localhost",
+    port: 8080,
+    transport: "tcp",
+  } as Deno.NetAddr,
+};
+
+function setup(): [string, AbortController, Record<string, string>[]] {
+  const requests: Record<string, string>[] = [];
+  const controller = new AbortController();
+  const listener = Deno.listen({ port: 0 });
+  const addr = listener.addr as Deno.NetAddr;
+  const server = `http://${addr.hostname}:${addr.port}/`;
+  controller.signal.addEventListener("abort", () => {
+    listener.close();
+  });
+  (async () => {
+    for await (const conn of listener) {
+      (async () => {
+        const httpConn = Deno.serveHttp(conn);
+        while (true) {
+          try {
+            const requestEvent = await httpConn.nextRequest();
+            if (requestEvent) {
+              requests.push(
+                ...(await requestEvent.request.text()).split("\n").filter((i) =>
+                  !!i
+                ).map((i) => Object.fromEntries(new URLSearchParams(i))),
+              );
+              await requestEvent.respondWith(
+                new Response(null, { status: 200 }),
+              );
+            } else {
+              return;
+            }
+          } catch {
+            return;
+          }
+        }
+      })();
+    }
+    console.log("end server");
+  })();
+  return [server, controller, requests];
+}
+
+Deno.test({
+  name: "createReporter - defaults",
+  async fn() {
+    const msgs: string[] = [];
+    const reporter = createReporter({
+      log(msg) {
+        msgs.push(msg);
+      },
+    });
+    await reporter(
+      new Request("http://localhost/"),
+      mockConn,
+      new Response(),
+      0,
+    );
+    assertEquals(msgs, [
+      "GA_TRACKING_ID environment variable not set. Google Analytics reporting disabled.",
+    ]);
+  },
+});
+
+Deno.test({
+  name: "createReporter - basic usage",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn() {
+    const [endpoint, controller, requests] = setup();
+    const logMsgs: string[] = [];
+    const report = createReporter({
+      id: "UA-XXXX-Y",
+      endpoint,
+      log(msg) {
+        logMsgs.push(msg);
+      },
+    });
+    const start = performance.now();
+    await report(
+      new Request("http://localhost/example"),
+      mockConn,
+      new Response("{}", {
+        status: 200,
+        headers: { "content-type": "application/json" },
+      }),
+      start,
+    );
+    await delay(1_200);
+    controller.abort();
+    assertEquals(logMsgs.length, 0);
+    assertEquals(requests.length, 1);
+    assertEquals(Object.keys(requests[0]), [
+      "v",
+      "tid",
+      "t",
+      "cid",
+      "uip",
+      "dl",
+      "srt",
+      "qt",
+    ]);
+    assertEquals(requests[0].v, "1");
+    assertEquals(requests[0].tid, "UA-XXXX-Y");
+    assertEquals(requests[0].t, "pageview");
+    assertEquals(requests[0].uip, "localhost");
+    assertEquals(requests[0].dl, "http://localhost/example");
+  },
+});
+
+Deno.test({
+  name: "createReportMiddleware - basic usage",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn() {
+    const [endpoint, controller, requests] = setup();
+    const logMsgs: string[] = [];
+    const mw = createReportMiddleware({
+      id: "UA-XXXX-Y",
+      endpoint,
+      log(msg) {
+        logMsgs.push(msg);
+      },
+    });
+    const ctx = createMockContext({
+      ip: "127.0.0.1",
+      path: "/example",
+    });
+    const next = createMockNext();
+    await mw(ctx, next);
+    await delay(1_200);
+    controller.abort();
+    assertEquals(logMsgs.length, 0);
+    assertEquals(requests.length, 1);
+    console.log(requests[0]);
+    assertEquals(Object.keys(requests[0]), [
+      "v",
+      "tid",
+      "t",
+      "cid",
+      "uip",
+      "dl",
+      "exd",
+      "exf",
+      "srt",
+      "qt",
+    ]);
+    assertEquals(requests[0].v, "1");
+    assertEquals(requests[0].tid, "UA-XXXX-Y");
+    assertEquals(requests[0].t, "pageview");
+    assertEquals(requests[0].uip, "127.0.0.1");
+    assertEquals(requests[0].dl, "http://localhost/example");
+  },
+});
